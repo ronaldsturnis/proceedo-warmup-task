bc94a4ec9257f642606698d01ef3d7bf76925cae
ronaldsturnis <ronalds.turnis@visma.com>
2022-09-05 14:41:20 +0300

    Added ESLint configuration

---
+++ src/app/src/components/popular-movies/popular-movies.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-movies',
+  templateUrl: './popular-movies.component.html',
+  styleUrls: ['./popular-movies.component.less']
+})
+export class PopularMoviesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
