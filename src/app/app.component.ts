c2e6f80eda7e0d902c25364fe20caeca188a484b
ronaldsturnis <ronalds.turnis@visma.com>
2022-09-16 15:44:40 +0300

    Refactored code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { paths } from './paths.const';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proceedo-warmup-task';
+  routingPaths?: any;
+
+  ngOnInit(): void {
+    this.routingPaths = paths;
+  }
 }
