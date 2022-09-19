9f219f80073c59cff6811fcbb4b94280e0eab5d8
ronaldsturnis <ronalds.turnis@visma.com>
2022-09-19 15:51:57 +0300

    Fixed test configuration

--- karma.conf.js
+++ karma.conf.js
@@ -10,40 +10,32 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-jasmine-seed-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reporters: [
-        { type: 'html', subdir: '.' },
-        { type: 'lcovonly', subdir: '.' },
-      ],
-      fixWebpackSourcePaths: true,
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
     },
-    reporters: ['progress', 'kjhtml', 'jasmine-seed'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/proceedo-warmup-task'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeDebugging'],
-    customLaunchers: {
-      CustomChromeHeadless: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
-      },
-      ChromeDebugging: {
-        base: 'Chrome',
-        flags: ['--remote-debugging-port=9333'],
-        debug: true,
-      },
-    },
+    browsers: ['Chrome'],
     singleRun: false,
-    // try to mitigate failing tests on CI
-    browserNoActivityTimeout: 100000,
-    browserDisconnectTimeout: 10000,
+    restartOnFileChange: true,
   });
 };
